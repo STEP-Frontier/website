f3909297b9d112cfe6c195c1357ed039232861ad
Penrose <huyang0904@outlook.com>
2025-03-15 18:15:16 +0900

    Editted next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    output: 'export',
+    basePath: '',
+    assetPrefix: '',
+  }
+  
 export default nextConfig;
