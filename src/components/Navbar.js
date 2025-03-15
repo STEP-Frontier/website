b4111ab615d71fef650aa0a6dd0cd8ea0f5147d1
Penrose <huyang0904@outlook.com>
2025-03-15 18:08:58 +0900

    Debug navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar({ isDark = false }) {
   const [scrollY, setScrollY] = useState(0);
@@ -29,14 +30,14 @@ export default function Navbar({ isDark = false }) {
       }}
     >
       <div className="max-w-[90rem] mx-auto px-4 md:px-8 lg:px-16 flex justify-between items-center">
-        <a href="/">
+        <Link href="/">
           <Image src="/images/logo.png" alt="STEP LOGO" width={200} height={100} className="w-32 sm:w-40 md:w-45 lg:w-50 h-auto" priority />
-        </a>
+        </Link>
         <div className="md:flex hidden space-x-16 font-bold">
-        <a href="/about" className="nav-link group">About</a>
-          <a href="/rocket" className="nav-link group">Rocket</a>
-          <a href="/cansat" className="nav-link group">CanSat</a>
-          <a href="/news" className="nav-link group">News</a>
+          <Link href="/about" className="nav-link group">About</Link>
+          <Link href="/rocket" className="nav-link group">Rocket</Link>
+          <Link href="/cansat" className="nav-link group">CanSat</Link>
+          <Link href="/news" className="nav-link group">News</Link>
         </div>
         <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>☰</button>
       </div>
@@ -44,10 +45,10 @@ export default function Navbar({ isDark = false }) {
       {/* 移动端菜单 */}
       {isOpen && (
         <div className="absolute top-full left-0 w-full bg-black flex flex-col items-center space-y-4 p-4">
-          <a href="/about" className="nav-link group">About</a>
-          <a href="/rocket" className="nav-link group">Rocket</a>
-          <a href="/cansat" className="nav-link group">CanSat</a>
-          <a href="/news" className="nav-link group">News</a>
+          <Link href="/about" className="nav-link group">About</Link>
+          <Link href="/rocket" className="nav-link group">Rocket</Link>
+          <Link href="/cansat" className="nav-link group">CanSat</Link>
+          <Link href="/news" className="nav-link group">News</Link>
         </div>
       )}
     </nav>
