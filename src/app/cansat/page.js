252931723e5eac3d2bc22956a1d53ea7fe377535
Penrose <huyang0904@outlook.com>
2025-03-24 15:18:58 +0900

    Init cansat page

---
+++ src/app/cansat/page.js
@@ -0,0 +1,14 @@
+import Hero from "@/components/Hero";
+
+export default function CanSat() {
+    return (
+        <div className="w-full">
+            <Hero
+                image_path="/images/cansat/cansat-hero-tmp.jpg"
+                hero_title="CANSAT PROJECTS"
+                subtitle="ただいま工事中でございます"
+                black_opacity={0.4}
+            />
+        </div>
+    )
+}
