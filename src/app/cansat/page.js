d1d4a370671775541fea34ca080deec8ef3e63db
nukanoto <me@nukanoto.net>
2025-10-14 22:18:14 +0900

    fix(/cansat): CanSat についての Hero へ basePath を適用

--- src/app/cansat/page.js
+++ src/app/cansat/page.js
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
 
+import { url } from "@/util/url-converter";
+
 export default function CanSat() {
     return (
         <div className="w-full">
             <Hero
-                image_path="/images/cansat/cansat-hero-tmp.jpg"
+                image_path={url("/images/cansat/cansat-hero-tmp.jpg")}
                 hero_title="CANSAT PROJECTS"
                 subtitle="ただいま工事中でございます"
                 black_opacity={0.4}
