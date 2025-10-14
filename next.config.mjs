18c8e3b36127eeec2d56d8abaa3fde82567e42db
nukanoto <me@nukanoto.net>
2025-10-14 18:02:31 +0900

    fix: use `/~step` as assetPrefix in production

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true, 
-    },
-    trailingSlash: true,
-    basePath: '',
-    assetPrefix: '',
-  }
-  
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: '',
+  assetPrefix: isProd ? '/~step' : ''
+}
+
 export default nextConfig;
