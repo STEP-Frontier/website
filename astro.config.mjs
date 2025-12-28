8ff29891838f45ae866ba7fc2aa097a6e701312a
nukanoto <me@nukanoto.net>
2025-12-28 13:31:01 +0900

    remove biome and add eslint and prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: { plugins: [tailwindcss()] },
-	integrations: [icon()],
+  vite: { plugins: [tailwindcss()] },
+  integrations: [icon()],
 });
