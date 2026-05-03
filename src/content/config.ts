1514498c379d93bbffd2415d00b906333f0b08b9
nukanoto <me@nukanoto.net>
2026-05-03 15:59:06 +0900

    remove js-yaml

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,21 @@ const newsCollection = defineCollection({
   }),
 });
 
+const aboutCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    representative_message: z.string(),
+    members: z.array(
+      z.object({
+        name: z.string(),
+        grade: z.number(),
+        department: z.string(),
+      }),
+    ),
+  }),
+});
+
 export const collections = {
+  about: aboutCollection,
   news: newsCollection,
 };
